3068391ec6643acc3f955c1e9e2fa8d1929f32eb
Hans Vandierendonck <hvandierendonck@acm.org>
2012-01-03 10:54:42 +0100

    Added new structure function_tags to this taskgraph.

--- scheduler/ecgtaskgraph.h
+++ scheduler/ecgtaskgraph.h
@@ -766,6 +766,8 @@ task_metadata::add_to_graph() {
 // ----------------------------------------------------------------------
 // Dependency handling traits
 // ----------------------------------------------------------------------
+// Whole-function dependency tags
+class function_tags : public function_tags_base { };
 
 // A fully serialized version
 class serial_dep_tags { };
